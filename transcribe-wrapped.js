fffbcae8f1f5d360158a8597d24ca3d2fb696264
Anthony <contact@anthonymayfield.com>
2022-11-06 17:17:35 +0100

    more updates

--- transcribe-wrapped.js
+++ transcribe-wrapped.js
@@ -7,52 +7,61 @@ const whisperPath = which.sync('whisper')
 l = console.log;
 
 async function transcribe(filename, path, language, model){
-  language = 'English' || language;
-  model = '' || model;
+  return new Promise((resolve, reject) => {
+    try {
+      language = 'English' || language;
+      model = '' || model;
 
-  let arguments = [path];
+      let arguments = [path];
 
-  if(language){
-    // set the language for whisper (if undefined with auto-detect and translate off that)
-    arguments.push('--language', language);
-  }
+      if(language){
+        // set the language for whisper (if undefined with auto-detect and translate off that)
+        arguments.push('--language', language);
+      }
 
-  if(model){
-    // set the language for whisper (if undefined with auto-detect and translate off that)
-    arguments.push('--model', model);
-  }
+      if(model){
+        // set the language for whisper (if undefined with auto-detect and translate off that)
+        arguments.push('--model', model);
+      }
 
-  if(filename){
-    arguments.push('-o', filename);
-  }
+      if(filename){
+        arguments.push('-o', filename);
+      }
 
-  // const path = filename;
+      // const path = filename;
 
-  // arguments.push(path);
+      // arguments.push(path);
 
-  console.log(arguments);
+      console.log(arguments);
 
-  // const bl = await spawn(whisperPath, arguments);
-  // l(bl);
-  // return true
+      // const bl = await spawn(whisperPath, arguments);
+      // l(bl);
+      // return true
 
 
-  // log a date when starting just to see how long it's taking
-  console.log(new Date())
-  
-  const ls = spawn(whisperPath, arguments);
+      // log a date when starting just to see how long it's taking
+      console.log(new Date())
 
-  ls.stdout.on('data', data => {
-    console.log(`stdout: ${data}`);
+      const ls = spawn(whisperPath, arguments);
+
+      ls.stdout.on('data', data => {
+        console.log(`stdout: ${data}`);
+      });
+
+      ls.stderr.on('data', data => {
+        console.log(`stderr: ${data}`);
+      });
+
+      ls.on('close', code => {
+        resolve(code);
+        console.log(`child process exited with code ${code}`);
+      });
+    } catch (err){
+      reject(err);
+    }
+
   });
 
-  ls.stderr.on('data', data => {
-    console.log(`stderr: ${data}`);
-  });
-
-  ls.on('close', code => {
-    console.log(`child process exited with code ${code}`);
-  });
 }
 
 module.exports = transcribe;
