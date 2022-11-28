3facbc4664230e6e8bf70c2a5b82897519d30dea
Anthony <contact@anthonymayfield.com>
2022-11-28 02:18:10 +0100

    bugfix attempt

--- libreTranslateWrapper.js
+++ libreTranslateWrapper.js
@@ -4,7 +4,14 @@ const l = console.log;
 // TODO: replace this with new instance
 const LTHost = process.env.LIBRETRANSLATE;
 
+l('LTHost');
+l(LTHost)
+
 const endpoint = LTHost + '/translate';
+
+l('endpoint');
+l(endpoint)
+
 
 process.on('unhandledRejection', (reason, promise) => {
   l(reason);
