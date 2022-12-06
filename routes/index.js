a9c13d7100fe96e877890001057d68f8683f1c23
Anthony <contact@anthonymayfield.com>
2022-12-06 02:50:45 +0100

    show latest stats

--- routes/index.js
+++ routes/index.js
@@ -20,10 +20,89 @@ const { languagesToTranslateTo, newLanguagesMap } = constants;
 // l('language name map');
 // l(newLanguagesMap.reverse())
 
+// one minute in milliseconds
+const oneMinute = 1000 * 60;
+
+const interval = oneMinute;
+
+global.siteStats = {}
+
+async function getTranscriptionData(){
+  let totalSeconds = 0;
+
+  const processDirectory = process.cwd();
+  const transcriptionsDirectory = `${processDirectory}/transcriptions`;
+  const transcriptionsDirectoryContents = await fs.readdir(transcriptionsDirectory);
+
+  // loop through all transcription directories
+  for (const transcriptionDirectory of transcriptionsDirectoryContents) {
+    // check if directory is directory
+    const directoryPath = `${transcriptionsDirectory}/${transcriptionDirectory}`;
+
+    // this is guaranteed to exist
+    const directoryStats = await fs.stat(directoryPath);
+
+    const isDirectory = directoryStats.isDirectory();
+
+    // only loop through if it's a directory
+    if (isDirectory) {
+      // check if directory has a processing_data.json file
+      const processingDataPath = `${directoryPath}/processing_data.json`;
+
+      // read processing_data.json file
+      // dont error if processingData doesn't exist
+      const processingDataExists = await fs.pathExists(processingDataPath);
+
+      if(!processingDataExists){
+        continue
+      }
+
+      let processingData, fileExistsButJsonError;
+      try {
+        processingData = JSON.parse(await fs.readFile(processingDataPath, 'utf8'));
+      } catch (err) {
+
+        // syntax error
+        fileExistsButJsonError = err.toString().includes('SyntaxError');
+
+        // delete the media if json error
+        if(fileExistsButJsonError){
+          continue
+        }
+      }
+
+      if(!processingData){
+        continue
+      }
+
+      const uploadDurationInSeconds = processingData.uploadDurationInSeconds;
+
+      if(uploadDurationInSeconds){
+        totalSeconds += uploadDurationInSeconds;
+      }
 
 
-// l('constants');
-// l(constants);
+    }
+  }
+
+  global.siteStats = {
+    totalSeconds,
+    amountOfTranscriptions: transcriptionsDirectoryContents.length,
+    humanReadableTime: forHumans(totalSeconds),
+  }
+
+  l('siteStats');
+  l(global.siteStats);
+}
+
+getTranscriptionData();
+
+// Schedule the directory reading operation at regular intervals
+setInterval(async () => {
+  getTranscriptionData();
+}, interval);
+
+
 
 const makeFileNameSafe = function(string){
   return filenamify(string, {replacement: '_' })
@@ -73,7 +152,8 @@ router.get('/', function(req, res, next) {
     title: 'Transcribe File',
     uploadPath,
     forHumans,
-    nodeEnv
+    nodeEnv,
+    siteStats: global.siteStats,
   });
 });
 
