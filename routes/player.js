3a89ffe918e927523fe47c33bfa02f3bd802e778
ahmedsaad3117 <ahmedsaad3117@gmail.com>
2022-12-23 16:27:25 +0200

    fix player router get

--- routes/player.js
+++ routes/player.js
@@ -7,30 +7,29 @@ const { newLanguagesMap } = require('../constants/constants');
 const { languagesToTranscribe } = require('../constants/constants');
 
 /** PLYR PLAYER **/
-router.get('/player/:filename', async function (req, res, next) {
+router.get("/player/:filename" , async function(req, res, next){
   try {
-    const fileNameWithoutExtension = req.params.filename;
+    const fileNameWithoutExtension = req.params.filename
 
     const processDirectory = process.cwd();
 
-    const filePathWithoutExtension = `/transcriptions/${fileNameWithoutExtension}/${fileNameWithoutExtension}`;
-
-    l('filePathWithoutExtension');
-    l(filePathWithoutExtension);
-
-    const containingFolder = `${processDirectory}/transcriptions/${fileNameWithoutExtension}`;
+    const containingFolder = `${processDirectory}/transcriptions/${fileNameWithoutExtension}`
 
     const processingDataPath = `${containingFolder}/processing_data.json`;
 
-    const processingData = JSON.parse(
-      await fs.readFile(processingDataPath, 'utf8')
-    );
+    const processingData = JSON.parse(await fs.readFile(processingDataPath, 'utf8'));
+
+
+    const filePathWithoutExtension = `/transcriptions/${fileNameWithoutExtension}/${processingData.directoryFileName}`;
+
+    l('filePathWithoutExtension')
+    l(filePathWithoutExtension);
 
     const translatedLanguages = processingData.translatedLanguages;
 
     // TODO: check that it doesn't include the original language? or it never will?
-    const languagesToLoop = newLanguagesMap.filter(function (language) {
-      return translatedLanguages.includes(language.name);
+    const languagesToLoop = newLanguagesMap.filter(function(language){
+      return translatedLanguages.includes(language.name)
     });
 
     l('processing data');
@@ -39,19 +38,30 @@ router.get('/player/:filename', async function (req, res, next) {
     l('languages to loop');
     l(languagesToLoop);
 
+    let allLanguages = languagesToLoop.slice();
+
+    allLanguages.push({
+      name: processingData.language,
+      languageCode: processingData.languageCode
+    })
+
+    l('all languages');
+    l(allLanguages);
 
     res.render('player/player', {
       filePath: filePathWithoutExtension,
       languages: languagesToTranscribe,
-      fileNameWithoutExtension, //
+      fileNameWithoutExtension,
       filePathWithoutExtension,
       processingData,
       title: processingData.filename,
       languagesToLoop,
+      allLanguages,
+      renderedFilename: req.params.filename
       // vttPath,
       // fileSource
-    });
-  } catch (err) {
+    })
+  } catch (err){
     l('err');
     l(err);
     res.send(err);
