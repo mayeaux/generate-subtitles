7beaadade4b97ef8161a4254799af0e58a96c7e6
Omar Shareef Abdul-Bar <omarshdev@gmail.com>
2023-01-17 20:35:09 +0200

    linting

--- constants/remoteServerConfig.js
+++ constants/remoteServerConfig.js
@@ -13,7 +13,7 @@ let remoteServerData = [{
   maxConcurrentJobs: 1,
 }]
 
-if(nodeEnv === 'production'){
+if (nodeEnv === 'production') {
   remoteServerData = [{
     endpoint: 'http://31.12.82.146:11460/api',
     maxConcurrentJobs: 2,
